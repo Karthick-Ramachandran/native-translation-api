7a25605bbbb00423a937e71e214ccf67b82e672b
Karthick <35869232+Karthick-Ramachandran@users.noreply.github.com>
2019-05-01 11:51:02 +0530

    Updated port

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.get('/:lang/:name', (req, res) => {
 
 
 })
-const port = process.env.PORT; 
+const port = 3000 || process.env.PORT; 
 app.listen(port, function(){
 console.log(`app started on port ${port}`)
 });
